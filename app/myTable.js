1d9c7e826716e17d5a81469071e3be5aff5c243f
Sergey Nefedov <nefedov38@gmail.com>
2024-02-29 22:43:38 +0300

    init

---
+++ app/myTable.js
@@ -0,0 +1,22 @@
+"use client"
+import {useState} from "react";
+
+export default function MyTable() {
+
+    const [headers, setHeaders] = useState(['Book', 'Author', 'Language', 'Published', 'Sales'])
+    const [data, setData] = useState([['A Tale of Two Cities', 'Charles Dickens', 'English', '1859', '200 million',], ['Le Petit Prince (The Little Prince)', 'Antoine de Saint-Exupéry', 'French', '1943', '150 million',], ["Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 'English', '1997', '120 million',], ['And Then There Were None', 'Agatha Christie', 'English', '1939', '100 million',], ['Dream of the Red Chamber', 'Cao Xueqin', 'Chinese', '1791', '100 million',], ['The Hobbit', 'J. R. R. Tolkien', 'English', '1937', '100 million']])
+
+
+    return (<table>
+        <thead>
+        <tr>
+            {headers.map((title, idx) => (<th key={idx}>{title}</th>))}
+        </tr>
+        </thead>
+        <tbody>
+        {data.map((row, idx) => (<tr key={idx}>
+                {row.map((cell, idx) => (<td key={idx}>{cell}</td>))}
+            </tr>))}
+        </tbody>
+    </table>);
+}
